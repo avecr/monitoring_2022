7ea5538fb6de30faecbd02ac6e53d628dc458a48
avecr <92572835+avecr@users.noreply.github.com>
2021-10-22 10:55:09 +0200

    Update R_First_code.r

--- R_First_code.r
+++ R_First_code.r
@@ -15,3 +15,29 @@ plot(water, fishes)
 # a table in R is called data frame 
 
 streams <- data.frame(water, fishes)
+
+# from now on we are going to import and/export data
+
+# setwd("/Users/anareis/OneDrive/MECF_R_Project/lab")
+setwd("/Users/anareis/OneDrive/MECF_R_Project/lab")
+
+# let's export our table!
+# https://www.rdocumentation.org/packages/utils/versions/3.6.2/topics/write.table
+write.table(streams, file="my_first_table.txt")
+
+# some collegues did send us a table How to import it in R?
+read.table("my_first_table.txt")
+
+# let's assign it ot an object inside R
+anatable <- read.table("my_first_table.txt")
+
+# the first statistics for lazy beautiful people
+summary(anatable)
+
+# Marta does not like water
+# Marta wants to get info only on fishes
+summary(anatable$fishes)
+
+# histogram 
+hist(anatable$fishes)
+hist(anatable$water)
