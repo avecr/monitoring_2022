2d99c1888912a116ab15483eeeffc02e637eaad7
avecr <92572835+avecr@users.noreply.github.com>
2021-10-29 11:01:45 +0200

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -6,3 +6,24 @@
 install.packages("raster")
 
 library(raster)
+
+setwd("/Users/anareis/OneDrive/MECF_R_Project/lab")
+
+# We are going to import satellite data
+
+l2011 <- brick("p224r63_2011.grd")
+
+# Objects in R cannot be numbers!
+
+l2011
+
+plot(l2011)
+
+# B1 is the reflectance in the blue band
+# B2 is the reflectance in the green band
+# B3 is the reflectance in the red band
+
+cl <- colorRampPalette(c("black","grey","light grey"))(100)
+plot(l2011, col=cl)
+
+plotRGB(l2011, r=3, g=2, b=1, stretch="Lin")
