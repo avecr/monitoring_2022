1c71cecc75b544eea4880fa55fc0fae31ab2d989
avecr <92572835+avecr@users.noreply.github.com>
2021-11-08 10:53:04 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -60,3 +60,53 @@ plot(l2011$B2_sre, col=clg)
 par(mfrow=c(2, 1))
 plot(l2011$B1_sre, col=clb)
 plot(l2011$B2_sre, col=clg)
+
+#---- day 3
+
+plot(l2011$B1_sre)
+
+# plot the blue band using a blue colorRampPalette
+clb <- colorRampPalette(c("dark blue","blue","light blue"))(100)
+plot(l2011$B1_sre, col=clb)
+par(mfrow=c(1,2)) #the first number is the number of rows in the multiframe
+
+# plot the blue and the green besides, with different colorRampPalette
+clg <- colorRampPalette(c("dark green", "green","light green"))(100)
+
+par(mfrow=c(1,2))
+plot(l2011$B1_sre, col=clb)
+plot(l2011$B2_sre, col=clg)
+
+# Exercise: put the plots one on the top of the other
+# invert the number of rows and the numberof columns 
+
+par(mfrow=c(2,1))
+plot(l2011$B1_sre, col=clb)
+plot(l2011$B2_sre, col=clg)
+
+# Exercise plot the four fist bands with two columns
+
+clr <- colorRampPalette(c("dark red", "red","pink"))(100)
+plot(l2011$B3_sre, col=clr)
+
+clNIR <- colorRampPalette(c("dark red", "orange","yellow"))(100)
+plot(l2011$B4_sre, col=clNIR)
+
+par(mfrow=c(2,2))
+plot(l2011$B1_sre, col=clb)
+plot(l2011$B2_sre, col=clg)
+plot(l2011$B3_sre, col=clr)
+plot(l2011$B4_sre, col=clNIR)
+
+# dev.off() to clear the plot window
+
+plotRGB(l2011, r=3, g=2, b=1, stretch="Lin") # natural colours
+plotRGB(l2011, r=4, g=3, b=2, stretch="Lin") # virtual colours
+plotRGB(l2011, r=3, g=4, b=2, stretch="Lin")
+plotRGB(l2011, r=3, g=2, b=4, stretch="Lin")
+
+par(mfrow=c(2,2))
+plotRGB(l2011, r=3, g=2, b=1, stretch="Lin")
+plotRGB(l2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(l2011, r=3, g=4, b=2, stretch="Lin")
+plotRGB(l2011, r=3, g=2, b=4, stretch="Lin")
