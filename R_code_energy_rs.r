c3f0951202e3c505449a40a7595e3db9aa093d34
avecr <92572835+avecr@users.noreply.github.com>
2021-11-19 11:14:31 +0100

    Create R_code_energy_rs.r

---
+++ R_code_energy_rs.r
@@ -0,0 +1,23 @@
+# R code for estimating energy in ecosystems
+# install_packages("raster")
+ 
+library(raster)
+ 
+# set the working directory
+setwd("/Users/anareis/OneDrive/MECF_R_Project/lab")
+ 
+# importing the data
+l1992 <- brick("defor1_.jpeg") # image of 1992
+ 
+# image of 1992
+l1992
+
+# Bands: defor1_.1, defor1_.2, defor1_.3
+# plotRGB
+plotRGB(l1992, r=1, g=2, b=3, stretch="Lin")
+
+# defor1_.1 = NIR
+# defor1_.2 = red
+# defor1_.3 = green
+
+
